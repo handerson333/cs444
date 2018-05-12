15cac79f61b29b322d5b224f37534b85d94f5a96
hayden <anderrob@oregonstate.edu>
2018-05-12 01:31:41 -0700

    Almost working just need to fix resource count bug

--- concurrency3/problem1/sharedResource.c
+++ concurrency3/problem1/sharedResource.c
@@ -2,7 +2,7 @@
 // Robert Hayden Anderson
 // CS444 Operating Systems 2
 // Prof: Kevin McGrath
-// Concurrency assignment 2
+// Concurrency assignment 3 problem 1 
 // sources:
 // http://pages.cs.wisc.edu/~remzi/Classes/537/Fall2008/Notes/threads-semaphores.txt
 // https://stackoverflow.com/questions/14888027/mutex-lock-threads
@@ -11,195 +11,83 @@
 // http://greenteapress.com/semaphores/LittleBookOfSemaphores.pdf
 // https://oregonstate.instructure.com/courses/1671300/assignments/7246802#submit
 // */
-// #include <stdlib.h>
-// #include <stdio.h>
-// #include <string.h>
-// #include <pthread.h>
-// #include <semaphore.h>
-// #include <unistd.h>
-// #include <time.h>
-// #include "rng.h"
-
-// #define TOTAL_THREADS 5
-// void* use_resource(void* users);
-
-
-// pthread_t users[TOTAL_THREADS];
-// pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
-// pthread_cond_t  cond = PTHREAD_COND_INITIALIZER;
-
-// //main function
-// int main() 
-// {
-
-
-//     for (int i = 0; i < TOTAL_THREADS; i++){
-//         pthread_create(&users[i], NULL, use_resource, &i);
-//     }
-//     //join all the threads
-//     for (int i = 0; i < TOTAL_THREADS; i++){
-//         pthread_join(users[i], NULL);
-//     }
-// 	return 0;
-// }
-
-// void* use_resource(void * temp){
-//     int threads = (int *)threads;
-//     int capacity = 3, i = 0;
-//     while(1){
-        
-//         if(pthread_mutex_trylock(&lock) == 0) {
-
-// 			while(threads == 3) {
-// 				pthread_cond_wait(&cond, &lock);
-// 				printf("\t%s\n","FULL");
-// 				pthread_mutex_unlock(&lock);
-// 			} 
-			
-// 			if(threads < 3) {
-// 				printf("%s %i \t %s%i\n","threads:",threads,"id:",threads);
-// 				threads++;
-				
-				
-// 				pthread_mutex_unlock(&lock);
-// 				sleep(rng(5,9));
-
-// 				pthread_mutex_lock(&lock);
-// 				threads--;
-// 				if(threads == 0)
-// 					pthread_cond_signal(&cond);
-// 				pthread_mutex_unlock(&lock);
-// 			}
-// 		}
-        
-//     }
-    
-
-
-
-// }
-
-
-// // //the entire program really...
-
-// // void* philosopher_thread(void* person)
-// // {
-// //     //recast the struct back to a struct
-// //     philosopher* ph = (struct philosopher*)person;
-// //     int x = 0;
-// // 	int think = 0;
-// // 	while(1) {
-// //         //if not thinking
-// // 		if(!think){
-// // 			while(1) {
-// //                 //continuously try to get a lock
-// // 				if(pthread_mutex_trylock(&lock) == 0) {
-// // 					x = rng(1);
-// // 					pthread_mutex_unlock(&lock);
-// // 					printf("%s is thinking for %d seconds\n", ph->name, x);
-// // 					sleep(x);
-// // 					break;
-// // 				}	
-// // 			}
-// // 		}
-// //         //try to pick up left fork
-// // 		if(pthread_mutex_trylock(&ph->fork_left_mutex) == 0) {
-// // 				printf("%s picks up fork %d\n", ph->name, ph->fork_left_number );
-// //             //if you can pick up left, try to pick up right fork
-// // 			if(pthread_mutex_trylock(&ph->fork_right_mutex) == 0) {
-// //                 //can pick up bothforks
-// // 				printf("%s picks up fork %d\n", ph->name, ph->fork_right_number );
-// // 				if(pthread_mutex_trylock(&lock) == 0) {
-// // 					x = rng(0);
-// // 					pthread_mutex_unlock(&lock);
-// // 					printf("%s is eating for %d seconds\n", ph->name, x);
-// // 					sleep(x);
-// // 					think= 0;
-// // 					printf("%s puts down fork %d\n", ph->name, ph->fork_left_number );
-// // 					printf("%s puts down fork %d\n", ph->name, ph->fork_right_number );
-// //                     pthread_mutex_unlock(&ph->fork_left_mutex);
-// // 					pthread_mutex_unlock(&ph->fork_right_mutex);
-// // 				}
-// // 			} 
-// //             //If youre done eating, put down both forks
-// //             else {
-// // 				think = 1;
-// // 				printf("%s puts down fork %d\n", ph->name, ph->fork_left_number);
-// // 				printf("%s puts down fork %d\n", ph->name, ph->fork_right_number );
-// //                 pthread_mutex_unlock(&ph->fork_left_mutex);
-// // 				pthread_mutex_unlock(&ph->fork_right_mutex);
-// // 			}
-// //         }
-// //         //put the left fork back down if you can't pick up both forks 
-// //         else {
-// // 			think = 1;	
-// // 			printf("%s puts down fork %d\n", ph->name, ph->fork_left_number );
-// //             pthread_mutex_unlock(&ph->fork_left_mutex);
-// // 		}
-// // 	}
-// // }
-
+#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <stdbool.h>
 #include <pthread.h>
 #include <semaphore.h>
-#include <time.h>
 #include <unistd.h>
 #include "rng.h"
-#define THREADS 12
+#define THREADS 10
 
-void* use_resource(void* arg);
-
+// Thread stuff
 pthread_t threads[THREADS];
 pthread_mutex_t mutex = PTHREAD_MUTEX_INITIALIZER;
 pthread_cond_t  cond = PTHREAD_COND_INITIALIZER;
-int shared = 0;
-bool waiting = false;
+// Global for amount of resources in use
+int resource = 0;
+
+// functions
+void* use_resource(void* void_thread);
 
 
-int main() 
-{
-
-	for(int i=0; i<THREADS; i++)
-		pthread_create(&threads[i], NULL, use_resource, &i);
+int main() {
+	printf("\nCurrently using %d threads\n\n", THREADS);
+	// create threads
+	for(int i=0; i<THREADS; i++){
+		pthread_create(&threads[i], NULL, use_resource, i);
+	}
+		pthread_join(threads[0], NULL);
 	
-	pthread_join(threads[0], NULL);
 
 	return 0;		
 }
 
-void* use_resource(void* temp)
-{
-	int thread = *((int *) temp);
-	int w_time = 0;
+// Use the resource
 
+void* use_resource(void* void_thread){\
+	// convert thread number back
+	int thread = (int*) void_thread;
+	int sleep_time = 0;
+	
+	//loop forever
 	while(1) {
-
-		sleep(rng(1,2));
+		// try mutex lock
 		if(pthread_mutex_trylock(&mutex) == 0) {
-
-			while(shared == 3) {
+			//if 3 resources in use, wait until signal to unlock
+			if (resource == 3){
+				printf("\nResource is locked\n\n");
+				while( resource != 0 ) {
+				sleep(3);
 				pthread_cond_wait(&cond, &mutex);
-				printf("\t%s\n","FULL");
 				pthread_mutex_unlock(&mutex);
-			} 
-			
-			if(shared < 3) {
-				printf("%s %i \t%s%i \t%s%i\n","SHARED:",shared,"sleep:",w_time,"thread:",thread);
-				shared++;
-				w_time = rng(1,2);
-				
+				} 
+			}
+			// If less than 3 resources in use
+			if( resource < 3 ) {
+				// get sleep time
+				sleep_time = rng(1,10);		
+				// add 1 to amount using resource
+				resource++;
+				printf("Thread %d is using the resource and sleeping for %d seconds\nResources currently in use: %d\n", thread, sleep_time, resource);
+				//unlock mutex
 				pthread_mutex_unlock(&mutex);
-				sleep(w_time);
-
+				// sleep while "using the resource"
+				sleep(sleep_time);
+				//lock mutex
 				pthread_mutex_lock(&mutex);
-				shared--;
-				if(shared == 0)
+				// no longer using resource, decrement resource
+				resource--;
+				printf("Thread %d is stopped using the resource\nResources currently in use: %d\n", thread, sleep_time, resource);
+				//if no reources in use then send signal to locked loop
+				if(resource == 0){
 					pthread_cond_signal(&cond);
+				}
 				pthread_mutex_unlock(&mutex);
 			}
 		}
+		// sleep to let user be able to see whats going on. It gets fast
+		sleep(rng(1,3));
 	}
 }
 
